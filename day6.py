a8f79aa780172668a92afac8a5aaee97c5de9b2c
David Winsor <davidjwinsor@gmail.com>
2025-04-17 17:50:43 -0400

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -3,11 +3,12 @@
 # ..........
 # ..#.......
 # .......#..
-# .........
+# ..........
 # .#..^.....
 # ........#.
-# .........
+# #.........
 # ......#...
+import copy
 
 FILEPATH = 'day6_input.txt'
 with open(FILEPATH, 'r') as file:
@@ -23,7 +24,6 @@ OPEN_ON = 'X'
 CLOSED = '#'
 
 for row_num, row in enumerate(LAB_MAP):
-    # print(row)
     for char in row:
         if char in MOVES:
             # Starting point
@@ -31,9 +31,12 @@ for row_num, row in enumerate(LAB_MAP):
             curr_position = [row_num, row.index(char), move_direction]
 
 LAB_SIZE = row_num
+# print(LAB_SIZE)
+
+starting_pos = curr_position.copy()
 
 
-def move_to_next_space(curr_pos):
+def move_to_next_space(curr_pos, lab_map):
     x = curr_pos[0]
     y = curr_pos[1]
     if curr_pos[2] == 0:
@@ -51,38 +54,50 @@ def move_to_next_space(curr_pos):
 
     if x < 0 or y < 0 or x > LAB_SIZE or y > LAB_SIZE:
         # The next move is out of bounds so stop
-        LAB_MAP[curr_pos[0]][curr_pos[1]] = OPEN_ON
+        lab_map[curr_pos[0]][curr_pos[1]] = OPEN_ON
         return False
 
-    value_of_position = LAB_MAP[x][y]
+    next_pos = (x, y, curr_pos[2])
+    value_of_position = lab_map[x][y]
+    if next_pos in unique_moves:
+        return None
 
     if value_of_position == CLOSED:
         if curr_pos[2] == 3:
             next_move_direction = 0
         else:
-            next_move_direction = curr_pos[2] + 1 
+            next_move_direction = curr_pos[2] + 1
         new_curr_position = (curr_pos[0], curr_pos[1], next_move_direction)
         return new_curr_position
 
     elif value_of_position == OPEN_OFF:
-        LAB_MAP[curr_pos[0]][curr_pos[1]] = OPEN_ON
+        lab_map[curr_pos[0]][curr_pos[1]] = OPEN_ON
         new_curr_position = (x, y, curr_pos[2])
         return new_curr_position
 
     elif value_of_position == OPEN_ON:
-        LAB_MAP[curr_pos[0]][curr_pos[1]] = OPEN_ON
+        lab_map[curr_pos[0]][curr_pos[1]] = OPEN_ON
         new_curr_position = (x, y, curr_pos[2])
         return new_curr_position
-    else:
-        return False
 
 
-while curr_position:
-    new_curr_position = move_to_next_space(curr_position)
-    curr_position = new_curr_position
+good_obstructions = 0
+# +1 lab size
+for x in range(LAB_SIZE+1):
+    for y in range(LAB_SIZE+1):
+        curr_position = starting_pos
+        unique_moves = {}
+        a = LAB_MAP[x][y]
+        if a == '.':
+            test_map = copy.deepcopy(LAB_MAP)
+            test_map[x][y] = '#'
+            while curr_position:
+                new_curr_position = move_to_next_space(curr_position, test_map)
+                if unique_moves.get(new_curr_position, 0) == 0:
+                    unique_moves[new_curr_position] = 1
+                if new_curr_position is None:
+                    good_obstructions += 1
+                    print(good_obstructions)
+                curr_position = new_curr_position
 
-sum = 0
-for row in LAB_MAP:
-    # print(row)
-    sum += row.count('X')
-print(sum)
+print(good_obstructions)
