5f591af946c99189fdb35500cf04a54e6ad049be
David Winsor <davidjwinsor@gmail.com>
2025-05-18 16:51:26 -0400

    Day 7 part 1

--- day7.py
+++ day7.py
@@ -1,77 +1,65 @@
-import math
-import copy
 
-FILEPATH = 'day7_sample_input.txt'
-clean_input = []
-with open(FILEPATH, 'r') as file:
-    for line in file:
-        line = line.strip().split(':')
-        test_case = int(line[0])
-        test_data = list(map(int, line[1].strip().split(' ')))
-        clean_input.append([test_case, test_data])
+def main():
+    FILEPATH = 'day7_input.txt'
+    clean_input = []
+    with open(FILEPATH, 'r') as file:
+        for line in file:
+            line = line.strip().split(':')
+            test_case = int(line[0])
+            test_data = list(map(int, line[1].strip().split(' ')))
+            clean_input.append([test_case, test_data])
+    #print(clean_input)
 
-print(clean_input)
+    operation_slot_combinations = {}
+    sum = 0
+    for row in clean_input:
+        number_of_slots = len(row[1])-1
+        answer = row[0]
+        numbers_list = row[1]
+        try:
+            opertations_list = operation_slot_combinations[number_of_slots]
+            if test_list(answer, numbers_list, opertations_list):
+                sum += answer
+        except KeyError:
+            seed_list =  [ ['+' for i in  range(0, number_of_slots)] ]
+            new_operations_list = create_new_lists(seed_list)
+            operation_slot_combinations[number_of_slots] = new_operations_list
+            if test_list(answer, numbers_list, new_operations_list):
+                sum += answer
+#    print(operation_slot_combinations)
+    print(sum)
+    return sum
 
-
-def left_to_right(num1, num2, symbol):
-    if symbol == '+':
-        return num1 + num2
-    elif symbol == '*':
-        return num1 * num2
-    else:
-        return None
-
-operators =  ['*', '+']
-
-for test_line in clean_input:
-    data = test_line[1]
-    # -1 because there is 1 less in between spaces then the length
-    operator_slots = len(data)-1
-    total_combinations = math.factorial(operator_slots)
+def create_new_lists(seed_lists_within_list):
+    for seed_list in seed_lists_within_list:
+        for i in range(0, len(seed_list)):
+            new_seed_list = seed_list.copy()
+            if new_seed_list[i] == '+':
+                new_seed_list[i] = '*'
+            elif new_seed_list[i] == '*':
+                new_seed_list[i] = '+'
+            else:
+                break
+            if new_seed_list not in seed_lists_within_list:
+                seed_lists_within_list.append(new_seed_list)
+    return seed_lists_within_list
     
-    print(total_combinations)
+def test_list(answer, numbers_list, operations_list):
+    for x in operations_list:
+        clean_list = []
+        for i in range(0, len(numbers_list)):
+            clean_list.append(numbers_list[i])
+            if i < len(x):
+                clean_list.append(x[i])
+        result = clean_list[0]                        
+        for i in range(0, len(clean_list)):           
+            if clean_list[i] == '+':
+                result += clean_list[i+1]
+            elif clean_list[i] == '*':
+                result *= clean_list[i+1]
+        if result == answer:
+            return True
+    return False
 
-
-def create_start_list(total_slots):
-    start_list = []
-    for i in range(0, total_slots):
-        start_list.append('+')
-    print(start_list)
-    return start_list 
-
-
-def create_lists(op_lists, total_combinations):
-    work_list = op_lists.copy()
-    print(type(op_lists))
-    for operator_list in op_lists:
-        for i in range(0, total_combinations):
-            if type(operator_list) is not list:
-                return
-            work_list = operator_list.copy()
-            work_list[i] = '*'
-            print(work_list)
-            if work_list not in list_of_operator_combinations:
-                list_of_operator_combinations.append(work_list)
-            work_list = operator_list.copy()
-    create_lists(work_list, total_combinations)
-
-
-list_of_operator_combinations = []
-test = create_start_list(3)
-list_of_operator_combinations.append(test)
-create_lists(list_of_operator_combinations, 3)
-
-print('Unique combinations\n{}'.format(list_of_operator_combinations))
-
-#print(clean_input)
-#def test_row(row):
-#    list_of_operator_combinations = []
-#    test_case_answer = row[0]
-#    numbers = row[1]
-#    operator_slots = len(numbers)-1
-#    create_lists(create_start_list(operator_slots), operator_slots)
-
-    
-
-
-
+if __name__ == "__main__":
+    main()
