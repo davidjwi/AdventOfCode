5617c8cf77ca2370e1b907dd1d393c2d6b07dfae
David Winsor <davidjwinsor@gmail.com>
2025-06-22 22:32:33 -0400

    Day 8 part 2 starting

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,18 +1,20 @@
-FILEPATH = 'input.txt'
+FILEPATH = 'sample_input.txt'
 
 
 class Node():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __str__(self):
         return f"({self.x}, {self.y})"
+
     def get_distance_to_another_node(self, otherNode):
         return (otherNode.x - self.x, otherNode.y - self.y)
+
     def is_same(self, otherNode):
         if self.x == otherNode.x and self.y == otherNode.y:
-          return True
-
+            return True
 
 
 def create_clean_input(filepath):
@@ -34,6 +36,33 @@ def create_clean_input(filepath):
 data = create_clean_input(FILEPATH)
 positions = data[1]
 clean_input = data[0]
+x_len = len(clean_input)
+y_len = len(clean_input[0])
+
+
+def is_oob(x_len, y_len, node):
+    print('Checking {}'.format(node))
+    if node[0] >= x_len or node[0] < 0 or node[1] >= y_len or node[1] < 0:
+        return False
+    else:
+        return True
+
+
+def get_inline_nodes(node1, node2, bounds):
+    '''
+    Gets all the nodes in line between 2 node points
+    '''
+    dist = node1.get_distance_to_another_node(node2)
+    print('Dist = {}'.format(dist))
+    x, y = bounds[0], bounds[1]
+    new_nodes = []
+    newNode1 = Node(node1.x - x, node1.y - y)
+    newNode2 = Node(node2.x + x, node2.y + y)
+    if not(newNode1.is_oob):
+        new_nodes.append(newNode1)
+    if not(newNode2.is_oob):
+        new_nodes.append(newNode2)
+    return False
 
 
 def create_antinodes(node1, allNodes, positions):
@@ -41,12 +70,36 @@ def create_antinodes(node1, allNodes, positions):
     for otherNode in allNodes:
         dist = node1.get_distance_to_another_node(otherNode)
         if dist == (0, 0):
+            print('Is 0,0')
             pass
         else:
-            newNode1 = (node1.x-dist[0], node1.y-dist[1])
-            newNode2 = (otherNode.x+dist[0], otherNode.y+dist[1])
-            new_antinodes.extend([newNode1, newNode2])
-    return new_antinodes 
+            oob = False
+            while oob is False:
+                print('Here')
+                print('node1 {}'.format(node1))
+                newNode1 = (node1.x-dist[0], node1.y-dist[1])
+                print('newNode1 {}'.format(newNode1))
+                print(newNode1)
+                if is_oob(x_len, y_len, newNode1) is False:
+                    new_antinodes.append(newNode1)
+                    node1 = newNode1
+                    print('node1 {}'.format(node1))
+            oob = False
+            while oob is False:
+                newNode2 = (otherNode.x+dist[0], otherNode.y+dist[1])
+                if is_oob(x_len, y_len, newNode2) is False:
+                    new_antinodes.append(newNode2)
+                    otherNode = newNode2
+    return new_antinodes
+
+
+def get_antinodes_till_oob(node, dist, len_x, len_y):
+    antinodes = []
+    oob = False
+    while oob is False:
+        newNode = (node.x-dist[0], node.y-dist[1])
+        if is_oob(len_x, len_y, newNode) is False:
+            antinodes.append(newNode)
 
 
 def process_positions(positions):
@@ -54,7 +107,8 @@ def process_positions(positions):
         all_new_antinodes = []
         if len(positions[key]) > 1:
             for node in positions[key]:
-                new_antinodes = create_antinodes(node, positions[key], positions)
+                new_antinodes = create_antinodes(
+                    node, positions[key], positions)
                 for node in new_antinodes:
                     if node not in all_new_antinodes:
                         all_new_antinodes.append(node)
@@ -76,7 +130,9 @@ def remove_oob_antinodes(x_len, y_len, positions):
         positions['#'].remove(val)
     return positions
 
-positions = remove_oob_antinodes(len(clean_input), len(clean_input[0]), positions)
+
+positions = remove_oob_antinodes(
+    len(clean_input), len(clean_input[0]), positions)
 
 
 print(positions)
