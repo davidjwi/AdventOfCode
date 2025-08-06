d4394e495f707306f46c93c6728ac5d8a735a184
David Winsor <david@Davids-MacBook-Pro.local>
2025-08-05 23:36:00 -0400

    Day 10 part 1

--- Day10/main.py
+++ Day10/main.py
@@ -1,75 +1,102 @@
-
 class Point:
-    def __init__(self, x, y, val):
+    def __init__(self, x: int, y: int, val: int):
         self.x = x
-        self.y = y
-        self.neighbor_points = {}
-        self.val = val
-
+        self. y = y
+        self.val = val 
+        self.neighbors = {}
+    
     def __repr__(self):
-        return f"Point({self.x}, {self.y}): {self.val}"
-
-    def add_neighbor_points(self, point, direction):
+        return f'({self.x},{self.y}): {self.val}'
+    
+    def add_neighbors(self, neighborPoint: "Point", direction: str):
+        # This is used to more easily link the current point to a neighbor
+        # and then link the neighbor back to the current point
         direction_opposites = {
             'north': 'south',
-            'south': 'north',
+            'south': 'north', 
             'east': 'west',
             'west': 'east'
         }
-        self.neighbor_points[direction] = point
-        point.neighbor_points[direction_opposites[direction]] = self
+        # Create a key(direction) and value(neighborPoint) in the
+        # currently-being-worked-on point's neighbor (self.neighbors) dictionary
+        self.neighbors[direction] = neighborPoint 
+        # Then set the neighboring point to have the current point as
+        # it's neighbor in the opposite direction
+        neighborPoint.neighbors[direction_opposites[direction]] = self
+        
 
-
-def create_topo_map(filepath: str) -> list:
+def create_map_from_file(FILEPATH: str) -> list:
+    # This will be the 2D list
     topo_map = []
-    with open(filepath, 'r') as file:
-        for x, line in enumerate(file):
-            points = []
-            for y, val in enumerate(line.strip()):
-                points.append(Point(x, y, val))
-            topo_map.append(points)
 
+    with open(FILEPATH, 'r') as input_file:
+
+        for x, row in enumerate(input_file):
+            row_of_points = []
+            row = row.strip()
+            for y, val in enumerate(row):
+                val = int(val)
+                NewPoint = Point(x, y, val)
+                row_of_points.append(NewPoint)
+
+            topo_map.append(row_of_points)
+
+    return topo_map
+
+def link_points_to_neighbors(topo_map: list) -> list:
+    # Iterate in order thru the 2d list and get the CurrPoint
     for x in range(0, len(topo_map)):
         for y in range(0, len(topo_map[0])):
-            point = topo_map[x][y]
-
+            CurrPoint = topo_map[x][y]
+            # This is like add/subtract to the CurrPoint's x,y coordinate
+            # 'north' = -1,0 | 'south' = 1,0 | 'east' = 0,1 | 'west' = 0,-1
+            # For the CurrPoint perform add_neighbors using the backward (west/applying 0,-1) point,
+            # then link the backward/west point back to the current point (forward/east) 
+            # Then do the same thing for looking upwards (north/applying 1,0) and
+            # also link the upwards point back down/south to the current point
+            # Only need to look back/west and up/north because the other directions will get linked along the way
             if y > 0:
-                point.add_neighbor_points(topo_map[x][y-1], 'west')
+                CurrPoint.add_neighbors(topo_map[x][y-1], 'west')
             if x > 0:
-                point.add_neighbor_points(topo_map[x-1][y], 'north')
+                CurrPoint.add_neighbors(topo_map[x-1][y], 'north')
+
     return topo_map
 
 
-def check_potential_trail(topo_map: list, position: tuple, good_trails: int) -> int | None:
-    x, y = position
-    point = topo_map[x][y]
-    neighbors = point.neighbor_points
-    for neighbor_point in neighbors:
-        Neighbor = neighbors[neighbor_point]
-        if int(Neighbor.val) == int(point.val) + 1:
-            print(f'Recurse on point {Neighbor}')
-            check_potential_trail(topo_map, (Neighbor.x, Neighbor.y), good_trails)
-            return None
-        elif int(point.val) == 9:
-            # Reached the end of a good trail?
-            print('At 9')
-            return 1 + good_trails
-        else:
-            # Cant be good trail
+def modifiedDFS(CurrPoint: Point, results: list) -> list:
+
+    neighboring_points = CurrPoint.neighbors
+
+    for NeighborPoint in neighboring_points.values():
+        #print(f'CurrPoint{CurrPoint} | NeighborVal{NeighborPoint}')
+        if NeighborPoint in results:
             pass
-    return None
+        elif NeighborPoint.val == CurrPoint.val + 1:
+            if NeighborPoint.val != 9:
+                modifiedDFS(NeighborPoint, results)
+            elif NeighborPoint.val == 9:
+#                print(f'Full trail ends at {NeighborPoint}')
+                results.append(NeighborPoint)
+    return results
 
 
-def main() -> int | None:
-    # Trails start on characters 0 and must go up by 1 for
-    # each up/down/left/right move until 9 is reached to be considered good
-    # Count the number of trails for each trail-head (0) starting point
-    # and sum all of those to create the answer score
-    topo_map = create_topo_map('sample_input.txt')
-    print(topo_map)
-    check_potential_trail(topo_map, (0, 0), 0)
-    return None
+def main():
+    FILEPATH = 'input.txt'
+    topo_map = create_map_from_file(FILEPATH)
+    topo_map = link_points_to_neighbors(topo_map)
+    answer = 0
+    for x in range(0, len(topo_map)):
+        for y in range(0, len(topo_map[0])):
+            CurrPoint = topo_map[x][y]
+            if CurrPoint.val == 0:
+                results = []
+                currPointAnswer= modifiedDFS(CurrPoint, results)
+#                print(f'Trailhead{CurrPoint} has {len(currPointAnswer)} trail(s)')
+                answer += len(currPointAnswer)
+
+    return answer
 
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    print(main())
