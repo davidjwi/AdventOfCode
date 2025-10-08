2509806beee463b5366127faaf86a590a5ea09e8
David Winsor <davidjwinsor@gmail.com>
2025-10-07 23:16:32 -0400

    Day 11 part 2 updated

--- Day11/main.py
+++ Day11/main.py
@@ -1,55 +1,48 @@
+from dataclasses import dataclass
+from functools import cache
+
+
+@dataclass(frozen=True)
 class Stone:
-    def __init__(self, val: int, blinks: int):
-        self.val = val
-        self.blinks = blinks
+    val: int
+    blinks: int
 
 
-num_cache = {}
-
-
-def process_num(val: int, num_cache: dict) -> list:
-    if val not in num_cache:
-        if val != 0:
-            str_val = str(val)
-            len_val = len(str_val)
-            if len_val % 2 == 0:
-                # floor divide
-                midIdx = len_val//2
-                left, right = str_val[:midIdx], str_val[midIdx:]
-                left, right = int(left), int(right)
-                num_cache[val] = [left, right]
-            else:
-                num_cache[val] = [val * 2024]
+@cache
+def process_num(val: int) -> list:
+    if val != 0:
+        str_val = str(val)
+        len_val = len(str_val)
+        if len_val % 2 == 0:
+            # floor divide
+            midIdx = len_val//2
+            left, right = str_val[:midIdx], str_val[midIdx:]
+            left, right = int(left), int(right)
+            return [left, right]
         else:
-            num_cache[val] = [1]
-
-    return num_cache[val]
-
-
-stone_cache = {}
+            return [val * 2024]
+    else:
+        return [1]
 
 
-def process_stone(stone: Stone, stone_cache: dict, result: int):
+@cache
+def process_stone(stone: Stone) -> int:
     val, blinks = stone.val, stone.blinks
-    key = (val, blinks)
 
     if blinks == 0:
-        return result + 1
+        return 1
 
-    if key not in stone_cache:
-        new_nums = process_num(val, num_cache)
-        if len(new_nums) == 2:
-            num1 = Stone(new_nums[0], blinks - 1)
-            num2 = Stone(new_nums[1], blinks - 1)
-            stone_cache[key] = process_stone(
-                num1, stone_cache, result) + process_stone(num2, stone_cache, result)
-        elif len(new_nums) == 1:
-            num = Stone(new_nums[0], blinks - 1)
-            stone_cache[key] = process_stone(num, stone_cache, result)
-    return stone_cache[key]
+    new_nums = process_num(val)
+    if len(new_nums) == 2:
+        num1 = Stone(new_nums[0], blinks - 1)
+        num2 = Stone(new_nums[1], blinks - 1)
+        return process_stone(num1) + process_stone(num2)
+    elif len(new_nums) == 1:
+        num = Stone(new_nums[0], blinks - 1)
+        return process_stone(num)
 
 
-def main():
+def main() -> None:
     blinks = 75
 
     filepath = 'input.txt'
@@ -59,10 +52,8 @@ def main():
         data = [Stone(int(x), blinks)
                 for x in file.readline().strip().split(' ')]
 
-    result = 0
-    for stone in data:
-        stones_at_zero_blinks = process_stone(stone, stone_cache, 0)
-        result += stones_at_zero_blinks
+    # generator expression inside sum
+    result = sum(process_stone(stone) for stone in data)
 
     print(result)
     return None
